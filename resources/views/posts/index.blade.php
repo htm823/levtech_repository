b97ec99d7d20118971ea0d2695c99201387d4866
htm823 <choco.cacao31@gmail.com>
2023-11-28 17:27:28 +0900

    Created authentication

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,33 +6,36 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
-    <body>
-        <h1>Blog Name</h1>
-        <p>[<a href='/posts/create'>create</a>]</p>
-        <div class='posts'>
-            @foreach ($posts as $post)
-                <div class='post'>
-                    <h2 class='title'>
-                        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-                    </h2>
-                    <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
-                    <p class='body'>{{ $post->body }}</p>
-                    <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        <button type="button" onclick="deletePost({{ $post->id }})">delete</button> 
-                    </form>
-                </div>
-            @endforeach
-        </div>
-        <script>
-            function deletePost(id) {
-                'use strict'
+    <x-app-layout>
+        <body>
+            <h1>Blog Name</h1>
+            <p>[<a href='/posts/create'>create</a>]</p>
+            <div class='posts'>
+                @foreach ($posts as $post)
+                    <div class='post'>
+                        <h2 class='title'>
+                            <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                        </h2>
+                        <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+                        <p class='body'>{{ $post->body }}</p>
+                        <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button type="button" onclick="deletePost({{ $post->id }})">delete</button> 
+                        </form>
+                    </div>
+                @endforeach
+            </div>
+            <p>ログインユーザー名：{{ Auth::user()->name }}</p>
+            <script>
+                function deletePost(id) {
+                    'use strict'
 
-                if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
-                    document.getElementById(`form_${id}`).submit();
+                    if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+                        document.getElementById(`form_${id}`).submit();
+                    }
                 }
-            }
-        </script>
-    </body>
+            </script>
+        </body>
+    </x-app-layout>
 </html>
