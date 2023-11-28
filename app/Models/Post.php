aef63ba6325402333824f11d60af8bc1b7f65e0a
htm823 <choco.cacao31@gmail.com>
2023-11-28 15:47:49 +0900

    Created categories.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,12 +12,18 @@ class Post extends Model
     use SoftDeletes;
     
     protected $fillable = [
-    'title',
-    'body',
+        'title',
+        'body',
+        'category_id'
     ];
     
-    public function getByLimit(int $limit_count = 10)
+    public function getPaginateByLimit(int $limit_count = 5)
     {
-        return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
+        return $this::with('category')->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
+    
+    public function category()
+    {
+    return $this->belongsTo(Category::class);
     }
 }
